f96ba1e02bfab6465ed0e252eb6429cf9500dde4
Nugo Bibilashvili <bibilashvili6@gmail.com>
2025-09-07 08:25:49 -0700

    swap values and count characters

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -7,24 +7,39 @@ const TextInput = () => {
   const [text2, setText2] = useState("");
   const [isCompared, setIsCompared] = useState(false);
 
+  const swapTexts = () => {
+    setText1(text2);
+    setText2(text1);
+  };
+
   return (
     <div className="pt-6 flex flex-col items-center font-helvetica lg:px-6">
       <div className="flex flex-col lg:flex-row small:gap-4 md:gap-[10px] w-full pb-8 items-center">
-        <DiffTextarea
-          text={text1}
-          onChange={setText1}
-          compareText={text2}
-          isCompared={isCompared}
-        />
-        <div className="small:rotate-90 lg:rotate-180">
+        <div className="w-full relative">
+          <DiffTextarea
+            text={text1}
+            onChange={setText1}
+            compareText={text2}
+            isCompared={isCompared}
+          />
+          <div className="absolute bottom-1 right-3 text-xs text-gray-500">
+            {text1.length} chars
+          </div>
+        </div>
+        <div className="small:rotate-90 lg:rotate-180" onClick={swapTexts}>
           <DoubleArrow />
         </div>
-        <DiffTextarea
-          text={text2}
-          onChange={setText2}
-          compareText={text1}
-          isCompared={isCompared}
-        />
+        <div className="w-full relative">
+          <DiffTextarea
+            text={text2}
+            onChange={setText2}
+            compareText={text1}
+            isCompared={isCompared}
+          />
+          <div className="absolute bottom-1 right-3 text-xs text-gray-500">
+            {text2.length} chars
+          </div>
+        </div>
       </div>
       <button
         onClick={() => setIsCompared(true)}
